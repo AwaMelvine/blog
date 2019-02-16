9c6c9330e036203895e85779da70618405264ebc
Awa Melvine <melvineawa@gmail.com>
2019-02-15 23:49:49 -0800

    Design auth pages
    - Design register and login pages
    - Make pages mobile responsive

---
+++ register.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="style.css">
+
+  <title>Register</title>
+</head>
+
+<body>
+
+  <!-- header -->
+  <header class="clearfix">
+    <div class="logo">
+      <!-- <img src="images/logo-placeholder.png" alt="Logo"> -->
+    </div>
+    <div class="fa fa-reorder menu-toggle"></div>
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="register.php">Sign up</a></li>
+        <li>
+          <a href="login.php">
+            <i class="fa fa-sign-in"></i>
+            Login
+          </a>
+        </li>
+
+        <!-- <li>
+          <a href="#" class="userinfo">
+            <i class="fa fa-user"></i>
+            Awa Melvine
+            <i class="fa fa-chevron-down"></i>
+          </a>
+          <ul class="dropdown">
+            <li><a href="#">Dashboard</a></li>
+            <li><a href="#" class="logout">logout</a></li>
+          </ul>
+        </li> -->
+      </ul>
+    </nav>
+  </header>
+  <!-- // header -->
+
+  <div class="auth-content">
+    <form action="register.php" method="post">
+      <h3 class="form-title">Register</h3>
+      <!-- <div class="msg error">
+        <li>Username required</li>
+      </div> -->
+      <div>
+        <label>Username</label>
+        <input type="text" name="username" class="text-input">
+      </div>
+      <div>
+        <label>Email</label>
+        <input type="email" name="email" class="text-input">
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password" name="password" class="text-input">
+      </div>
+      <div>
+        <label>Confirm Password</label>
+        <input type="password" name="passwordConf" class="text-input">
+      </div>
+      <div>
+        <button type="submit" name="register-btn" class="btn">Register</button>
+      </div>
+      <p class="auth-nav">Or <a href="login.php">Sign In</a></p>
+    </form>
+  </div>
+
+  <!-- JQuery -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+  <script src="scripts.js"></script>
+
+</body>
+
+</html>
